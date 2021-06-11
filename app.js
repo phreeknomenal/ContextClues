dcf4b961783ab9217ef1a1eac35dbbbfbaa6e046
unknown <xlanimation@gmail.com>
2021-06-11 13:34:59 -0500

    added comments

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 $(document).ready(() => {
 
+    // Arrays
     var friends = ['Chris', 'Cameron', 'Marques', 'James', 'John'];
     var locations = ['Asgard', 'Genosha', 'Krakoa', 'Attilan', 'Zenn-La', 'Latveria', 'Atlantis', 'Kun-Lun', 'Madripoor', 'Knowhere'];
     var weapons = ['Mjolnir', 'the Ebony Blade', 'All-Black', 'a Pistol', 'the Infinity Guantlet', 'Repulsors', 'Arrows', 'Fists', 'The Shield', 'Vibranium', 'Adamatium', 'Uru', 'Web-Shooters', 'Symbiotes', 'Drones', 'AIs', 'Claws', 'Swords', 'Bombs', 'Knives'];
 
-    // for (var i = 1; i <= 100; i++) {
-    //     var h3 = document.createElement('h3');
-    //     $('main').append(h3);
-    //     h3.innerText = `Accusations ${i}:`
-    //     $(h3).click(getAlert(i));
-    // }
+    // For loop that prints 100 h3 elements
     for (var i = 1; i <= 100; i++) {
         var $h3 = $("<h3>Accusation " + i + "</h3>");
         $('#container').append($h3);
+        // add listener for click and triggers getAlert function
+        // pass in {i} to get click instance of i
         $($h3).click(getAlert(i));
     }
 
+    // Get alert loop
     function getAlert(i) {
+        // Modulur math
         var friend = friends[i % 5];
         var location = locations[i % 10];
         var weapon = weapons[i % 20];
 
-        function accuAlert() {
+        // Compile the alert message in plug in variables. 
+        function compAlert() {
             alert(`Accusation ${i}: I accuse ${friend}, with ${weapon}, in ${location}`);
         }
-        return accuAlert;
+        // send results back to getAlert.
+        return compAlert;
         
     }
 
